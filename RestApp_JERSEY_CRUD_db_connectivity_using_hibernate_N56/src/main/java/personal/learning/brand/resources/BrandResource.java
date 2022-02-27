b453dcc3bbdc02536f3d238b3fa7c6600dbf3df7
aman-1998 <mishraamankpa@gmail.com>
2022-02-28 02:32:16 +0530

    Full Stack Development

---
+++ RestApp_JERSEY_CRUD_db_connectivity_using_hibernate_N56/src/main/java/personal/learning/brand/resources/BrandResource.java
@@ -0,0 +1,72 @@
+package personal.learning.brand.resources;
+
+import java.util.List;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import personal.learning.brand.Brand;
+import personal.learning.brand.services.BrandService;
+import personal.learning.status.Status;
+
+@Path("/showroom")
+public class BrandResource {
+
+	BrandService brandService = new BrandService();
+	
+	@GET
+	@Path("/brands")
+	@Produces(MediaType.APPLICATION_XML)
+	public List<Brand> getBrands() {
+		return brandService.getAllBrands();
+	}
+	
+	@GET
+	@Path("/brand/{brandId}")
+	@Produces(MediaType.APPLICATION_XML)
+	public Brand getBrand(@PathParam("brandId") int brandId) {
+		return brandService.getBrand(brandId);
+	}
+	
+	@POST
+	@Path("/createBrand")
+	@Consumes(MediaType.APPLICATION_XML)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String createBrand(Brand brand) {
+		boolean isCreated = brandService.createBrand(brand);
+		if(!isCreated) {
+			return "creation is unsuccessful";
+		}
+		return "Created successfully";
+	}
+	
+	@PUT
+	@Path("/updateBrand/{brandId}")
+	@Consumes(MediaType.APPLICATION_XML)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String updateBrand(@PathParam("brandId") int brandId, Brand brand) {
+		boolean isUpdated = brandService.updateBrand(brandId, brand);
+		if(!isUpdated) {
+			return "Updation is unsuccessful";
+		}
+		return "Updated successfully";
+	}
+	
+	@DELETE
+	@Path("/deleteBrand/{brandId}")
+	@Produces(MediaType.TEXT_PLAIN)
+	public String deleteBrand(@PathParam("brandId") int brandId) {
+		boolean isDeleted = brandService.deleteBrand(brandId);
+		if(!isDeleted) {
+			return "Deletion is unsuccessful";
+		}
+		return "Deleted successfully";
+	}
+}
