a9e043ba94915e2612bef9d7cc1b32dcc97f2ead
aman-1998 <mishraamankpa@gmail.com>
2022-04-25 01:39:52 +0530

    Full Stack Development

---
+++ RestApp_JERSEY_Building_Client_post_part_3_N85/src/main/java/personal/learning/resources/Brand.java
@@ -0,0 +1,17 @@
+package personal.learning.resources;
+
+public class Brand {
+	private String brandName;
+
+	public Brand(String brandName) {
+		this.brandName = brandName;
+	}
+
+	public String getBrandName() {
+		return brandName;
+	}
+
+	public void setBrandName(String brandName) {
+		this.brandName = brandName;
+	}
+}
---
+++ RestApp_JERSEY_Building_Client_post_part_3_N85/src/main/java/personal/learning/resources/ClientBuilding.java
@@ -0,0 +1,30 @@
+package personal.learning.resources;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.Invocation.Builder;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("/testClient")
+public class ClientBuilding {
+	
+	@GET
+	@Path("/test")
+	@Produces(MediaType.TEXT_PLAIN)
+	public Response test() {
+		Client client = ClientBuilder.newClient();
+		WebTarget baseUrl = client.target("http://localhost:8080/RestApp_JERSEY_HATEOAS_part_4_N77/showroom/brand");
+		//WebTarget extendedUrl = baseUrl.path("{brandId}");
+		//WebTarget finalUrl = extendedUrl.resolveTemplate("brandId", brandId);
+		Builder builder = baseUrl.request(MediaType.APPLICATION_JSON);
+		Response response = builder.post(Entity.json(new Brand("Tesla")));
+		return response;
+	}
+}
