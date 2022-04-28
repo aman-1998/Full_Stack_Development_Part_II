f37910f149132bdc5bef10f71b1384a18deabde0
aman-1998 <mishraamankpa@gmail.com>
2022-04-29 02:36:58 +0530

    Full Stack Development

---
+++ RestApp_JERSEY_Building_Client_post_part_4_N86/src/main/java/personal/learning/resources/Brand.java
@@ -0,0 +1,32 @@
+package personal.learning.resources;
+
+public class Brand {
+	private String brandName;
+	private String brandId;
+
+	public Brand(String brandName) {
+		this.brandName = brandName;
+	}
+
+	public Brand(String brandName, String brandId) {
+		this.brandName = brandName;
+		this.brandId = brandId;
+	}
+
+	public String getBrandName() {
+		return brandName;
+	}
+
+	public void setBrandName(String brandName) {
+		this.brandName = brandName;
+	}
+
+	public String getBrandId() {
+		return brandId;
+	}
+
+	public void setBrandId(String brandId) {
+		this.brandId = brandId;
+	}
+	
+}
