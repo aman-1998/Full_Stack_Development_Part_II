6b971b782bdcccb8f09a24fd9dbb892730714727
aman-1998 <mishraamankpa@gmail.com>
2022-12-13 00:00:03 +0530

    Bcrypt password encoder

---
+++ Spring5_SpringSecurity_part13_JDBC_Bcrypt_N110/src/main/java/personal/learning/config/AppConfig.java
@@ -0,0 +1,39 @@
+package personal.learning.config;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages="personal.learning")
+public class AppConfig implements WebMvcConfigurer {
+	
+	@Bean
+	public ViewResolver viewResolver() {
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		viewResolver.setPrefix("/WEB-INF/view/");
+		viewResolver.setSuffix(".jsp");
+		return viewResolver;
+	}
+	
+	@Bean
+	public MessageSource messageSource() {
+		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+		messageSource.setBasename("messages");
+		return messageSource;
+	}
+	
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/additional/**").addResourceLocations("/WEB-INF/template/");
+	}
+	
+}
---
+++ Spring5_SpringSecurity_part13_JDBC_Bcrypt_N110/src/main/java/personal/learning/config/SpringSecurityConfig.java
@@ -0,0 +1,133 @@
+package personal.learning.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Autowired
+	private DataSource dataSource;
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		
+		auth.jdbcAuthentication()
+			.dataSource(dataSource)
+			.usersByUsernameQuery("select username, password, enabled from my_users where username=?")
+			.authoritiesByUsernameQuery("select username, authority from my_authorities where username=?");
+		
+		/*
+		 * Tables in Oracle database:-
+		 * ---------------------------
+		 
+		   CREATE TABLE "MY_USERS" (
+    			"USERNAME"   VARCHAR2(128),
+    			"PASSWORD"   VARCHAR2(128) NOT NULL,
+    			"ENABLED"    CHAR(1 BYTE) NOT NULL,
+    			CHECK ( enabled IN ('Y','N') ),
+    			PRIMARY KEY ( "USERNAME" )
+			);
+			
+			CREATE TABLE "MY_AUTHORITIES" (
+    			"USERNAME"    VARCHAR2(128) NOT NULL,
+    			"AUTHORITY"   VARCHAR2(128) NOT NULL,
+    			CONSTRAINT "MY_AUTHORITIES_UNIQUE" UNIQUE ( "USERNAME","AUTHORITY" ),
+    			CONSTRAINT "MY_AUTHORITIES_FK1" FOREIGN KEY ( "USERNAME" )
+        			REFERENCES "MY_USERS" ( "USERNAME" )
+			);
+			
+			
+			Table MY_USERS:-
+			
+			USERNAME    PASSWORD                                                       Enabled                                                                                                                      E
+			----------- -------------------------------------------------------------- --------
+			aman        $2a$10$lz74m/L956k3NIivbAaCyeB.c2cKARo0r0hlkOEsmDpVneGcvzJDC    Y                                                                                                                      Y
+			madhav      $2a$10$kj60XzuJykItJ4egNUerw.U8rx2CtMwpwucoXw6I7hAKuKIEaLZ82    Y                                                                                                                     Y
+			rakhi       $2a$10$XPzfjC6bpRz6X2AO9Dl8QetGCM8UWFOrgPifs.B1BJj5cdM4HpYN6    Y                                                                                                                     Y
+			nitu        $2a$10$mpLpVq0IUya4auZUQN3U4ejt/WaLaGXnk/wqoNIR00SHBjkQtXre2    Y
+			
+			
+			Note:- Here, the passwords used for all users in 'test123'
+			
+			
+			Table MY_AUTHORITIES:-
+			
+			USERNAME     AUTHORITY                                                                                                                       
+			----------- --------------
+			aman        ROLE_TEACHER                                                                                                                    
+			madhav      ROLE_STUDENT                                                                                                                    
+			nitu        ROLE_ADMIN                                                                                                                      
+			nitu        ROLE_TEACHER                                                                                                                    
+			rakhi       ROLE_ADMIN
+			
+		 */
+	}
+
+	/*
+	 * Below method can also be used
+	 */
+//	@Autowired
+//	public void configAuthentication(AuthenticationManagerBuilder auth) throws Exception {
+//		
+//		  auth.jdbcAuthentication().dataSource(dataSource)
+//			.usersByUsernameQuery(
+//				"select username, password, enabled from users where username=?")
+//			.authoritiesByUsernameQuery(
+//				"select username, authority from authorities where username=?");
+//	}	
+	
+	protected void configure(HttpSecurity http) throws Exception {
+		
+		/*
+		 * 1. Resources from css, js and images folder are allowed and we don't need to authentication for that.
+		 * 2. Any url with pattern "/notice/**" and "/holiday/**" can be accessed by users with role "STUDENT", "TEACHER", "ADMIN".
+		 * 	  But without authentication these urls won't be accessible. And if we click on that url without being authenticated,
+		 * 	  we will be redirected to the login-page.
+		 * 3. Any url with pattern "/performance" can be accessed by users with role "TEACHER"
+		 * 4. Any url with pattern "/pendingFees" can be accessed by users with role "ADMIN"
+		 * 5. Any url for which there is some role assigned, if we try to access that url without logging in then we will be redirected to the login page.
+		 * 	  After logging in successfully we will either be directly redirected to the page that we tried to access earlier or to the Access-Denied Page depending on the role of the logged in user. But if we click on "Login" button from any page, we
+		 *    will be redirected to login page and after logging in successfully we will be redirected to "/" url or the url specified in 
+		 *    MySpringMvcDispatcherServletInitializer class under method getServletMappings().
+		 * 6. After logout we will be redirected to login page (This is the default behavior). In the login page here we have a Home button.
+		 * 7. As a logged in user if we try to access any url which is meant to be used by a user with different role then we will be redirected to the Access-Denied page.
+		 * 
+		 * Note:- The behaviors mentioned here are default.
+		 */
+		
+		http.authorizeRequests()
+			
+			.antMatchers("/notice/**", "/holiday/**").authenticated()
+			// or // .antMatchers("/notice/**", "/holiday/**").hasAnyRole("STUDENT", "TEACHER", "ADMIN")
+			
+			.antMatchers("/performance/**").hasRole("TEACHER")
+			.antMatchers("/pendingFees/**").hasRole("ADMIN")
+			.and()
+			.formLogin()
+			.loginPage("/myLogin") //GET
+			.loginProcessingUrl("/authenticateUser") // POST
+			.permitAll()
+			.and()
+			.logout()
+			.permitAll()
+			.and()
+			.exceptionHandling().accessDeniedPage("/accessDenied");
+	}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+}
